2128c208032c741818bfd4ecb7d9a1a0972dc9f7
Rui Peng <pengrui1993@gmail.com>
2016-02-25 17:32:31 +0800

    TDD #1

--- spec/main_spec.js
+++ spec/main_spec.js
@@ -6,26 +6,15 @@ var sinonChai = require("sinon-chai");
 var expect = chai.expect;
 chai.use(sinonChai);
 
-var main = require("../lib/main.js");
+var fizzbuzz = require("../lib/main.js");
 
+describe("FizzBuzz", function() {
 
-describe("测试描述", function(){
-    sinon.spy(console, 'log');
-
-    it("测试用例1", function(){
-
-        var result = main();
-        var expect_string = '';
-        
-        expect(expect_string).to.equal(result);
+    it(".say should return Fizz when input is multple of a", function() {
+      var fb = fizzbuzz(3, 5, 7);
+      expect(fb.say(3)).to.equal("Fizz");
+      expect(fb.say(6)).to.equal("Fizz");
+      expect(fb.say(9)).to.equal("Fizz");
     });
 
-    it("测试用例2", function(){
-
-        main();
-        var result = _.flatten(console.log.args).join("\n");
-        var expect_string = '';
-
-        expect(expect_string).to.equal(result);
-    });
 });
