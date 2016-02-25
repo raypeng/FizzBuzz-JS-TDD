cce942143df272199cd4d35e01832fad34b0e9c8
Rui Peng <pengrui1993@gmail.com>
2016-02-25 17:41:29 +0800

    TDD #3

--- spec/main_spec.js
+++ spec/main_spec.js
@@ -10,18 +10,24 @@ var fizzbuzz = require("../lib/main.js");
 
 describe("FizzBuzz", function() {
 
+    var fb = fizzbuzz(3, 5, 7);
+
     it(".say should return Fizz when input is multple of a only", function() {
-      var fb = fizzbuzz(3, 5, 7);
       expect(fb.say(3)).to.equal("Fizz");
       expect(fb.say(6)).to.equal("Fizz");
       expect(fb.say(9)).to.equal("Fizz");
     });
 
     it(".say should return Buzz when input is multple of b only", function() {
-      var fb = fizzbuzz(3, 5, 7);
       expect(fb.say(5)).to.equal("Buzz");
       expect(fb.say(10)).to.equal("Buzz");
       expect(fb.say(20)).to.equal("Buzz");
+    });
+
+    it(".say should return Whizz when input is multple of c only", function() {
+      expect(fb.say(7)).to.equal("Whizz");
+      expect(fb.say(14)).to.equal("Whizz");
+      expect(fb.say(28)).to.equal("Whizz");
     });
 
 });
